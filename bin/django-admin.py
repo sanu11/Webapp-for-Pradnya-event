7396baa49aa4e7ee93e1d060f08bd25e141618b7
Sanika Shah <sanikashah1110@gmail.com>
2018-10-17 02:07:25 -0700

    wsgiref added

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sanika/Sanika/Undergrad Projects/Webapp-for-Pradnya-event/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
